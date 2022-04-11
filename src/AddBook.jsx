64a7cea526fa05b206177b136b1f6cfbaf804b48
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-04-12 00:30:35 +0200

    fertig

--- src/AddBook.jsx
+++ src/AddBook.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
+import { BookContext } from './BookContext'
 
 const AddBook = () => {
+const [name,setName] =useState('')
+const [price,setPrice] =useState('')
+const [books,setBooks] = useContext(BookContext)
+
+
+
+const savebook= (e) =>{
+    e.preventDefault()
+    setBooks(prevdata=>[...prevdata,{name:name,price:price}])
+    setName('')
+    setPrice('')
+
+}
+
   return (
-    <div>AddBook</div>
+    <div>
+        <form onSubmit={savebook}>
+            <input type='text' placeholder='kitap adi' value={name} onChange={(e)=>setName(e.target.value)}/>
+            <input type='text' placeholder='kitap fizati' value={price} onChange={(e)=>setPrice(e.target.value)}/>
+            <button disabled={!name}>Add</button>
+        </form>
+    </div>
   )
 }
 
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
+import { BookContext} from './BookContext'
+import './Navbar.css'
 
 const Nav = () => {
+
+const [books,setBooks] =useContext(BookContext)
+
   return (
-    <div>Nav</div>
+    <div className='navbar'>
+        <h1>LOGO</h1>
+        <p>Kitapliktaki kitap sayisi: {books.length}</p>
+
+
+
+
+    </div>
   )
 }
 
